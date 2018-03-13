3d06d9b82da06a5cd3dc1f590d237a0b8071efdb
Kusnadi <kus.underdos@gmail.com>
2018-03-13 21:01:26 +0700

    First Commit

---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/login', function(req, res, next) {
+  let result = {
+    status: 0,
+    message: 'Success',
+    data: {
+      name: 'Kusnadi',
+      email: 'kus.underdos@gmail.com',
+      phone: '085780621341'
+    }
+  }
+
+  res.send(result);
+})
+
+module.exports = router;
